6682f13ef49d925bf4863e5eb4a08bafb568bb2e
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-04-21 22:26:18 +0200

    models and databse are set

--- book/models.py
+++ book/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.IntegerField()
+    added = models.DateTimeField(auto_now_add=True)
+    isread = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
--- book/views.py
+++ book/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def booklist(request):
+    return HttpResponse('Book List')
