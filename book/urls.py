694561e657e5d814ac627d899bca4c520fc46197
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-04-22 13:19:50 +0200

    book detail added

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookList
+from .views import BookList, BookDetail
 
 urlpatterns = [
     path('', BookList.as_view(), name='books'),
+    path('book/<int:pk>', BookDetail.as_view(), name='book_detail'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Book
 
 # Create your views here.
 
 class BookList(ListView):
     model = Book
+    context_object_name = 'books'
+
+class BookDetail(DetailView):
+    model = Book   
+    context_object_name = 'book' 
+    
     
 
