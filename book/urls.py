184da853119d2fbee5a421165e9f4075a05b4976
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-04-23 15:05:29 +0200

    Delete book added

--- book/urls.py
+++ book/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BookList, BookDetail, BookCreate, BookUpdate
+from .views import BookList, BookDetail, BookCreate, BookUpdate, BookDelete
 
 urlpatterns = [
     path('', BookList.as_view(), name='books'),
     path('book/<int:pk>', BookDetail.as_view(), name='book_detail'),
     path('book-create', BookCreate.as_view(), name='book-create'),
     path('book-update/<int:pk>', BookUpdate.as_view(), name='book-update'),
+    path('book-delete/<int:pk>', BookDelete.as_view(), name='book-delete'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Book
@@ -24,7 +24,11 @@ class BookCreate(CreateView):
 class BookUpdate(UpdateView):
     model = Book 
     fields = '__all__'  
-    success_url = reverse_lazy('books')   
-    
+    success_url = reverse_lazy('books') 
+
+class BookDelete(DeleteView):
+    model = Book
+    context_object_name = 'book'
+    success_url = reverse_lazy('books') 
     
 
