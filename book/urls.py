09a58cbd2bcba74e85c4e9410b9a3fa3b32753a5
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-04-23 14:48:35 +0200

    Book Update added

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookList, BookDetail, BookCreate
+from .views import BookList, BookDetail, BookCreate, BookUpdate
 
 urlpatterns = [
     path('', BookList.as_view(), name='books'),
     path('book/<int:pk>', BookDetail.as_view(), name='book_detail'),
     path('book-create', BookCreate.as_view(), name='book-create'),
+    path('book-update/<int:pk>', BookUpdate.as_view(), name='book-update'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from .models import Book
@@ -20,6 +20,11 @@ class BookCreate(CreateView):
     model = Book   
     fields = '__all__'  
     success_url = reverse_lazy('books')
+
+class BookUpdate(UpdateView):
+    model = Book 
+    fields = '__all__'  
+    success_url = reverse_lazy('books')   
     
     
 
