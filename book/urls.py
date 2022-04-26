41812bb3fc298ff9e6584ba7e6f9358476f23af4
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-04-26 03:39:13 +0200

    updated

--- book/urls.py
+++ book/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BookList, BookDetail, BookCreate, BookUpdate, BookDelete, CustomLoginView
+from .views import BookList, BookDetail, BookCreate, BookUpdate, BookDelete, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
     path('', BookList.as_view(), name='books'),
     path('book/<int:pk>', BookDetail.as_view(), name='book_detail'),
     path('book-create', BookCreate.as_view(), name='book-create'),
